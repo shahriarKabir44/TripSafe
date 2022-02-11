d6b25c58e6a6fc5f3fb14d94f17c573463b4b45a
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-02-12 02:48:16 +0600

    Add project files.

---
+++ TripSafe/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TripSafe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public ActionResult EmployeeLogin()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
