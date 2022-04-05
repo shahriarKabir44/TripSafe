abbd74a1ec1f353957b6136de782a5297d8be109
Md Shahriar Kabir <68655623+shahriarKabir44@users.noreply.github.com>
2022-04-05 12:56:49 +0600

    added new models

---
+++ TripSafe/Models/Node.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripSafe.Models
+{
+    public class Node
+    {
+        public int routeIdId { get; set; }
+        public int terminalId { get; set; }
+        public int stoppageIndex { get; set; }
+    }
+}
---
+++ TripSafe/Models/Route.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripSafe.Models
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public int start_terminal { get; set; }
+        public int end_terminal { get; set; }
+    }
+}
