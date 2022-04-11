52905f40622740dd1ccb46f4521c40e33f198b00
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-04-11 13:14:43 +0600

    fixed project

--- TripSafe/Properties/AssemblyInfo.cs
+++ TripSafe/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8717ddc6-b864-4b90-a822-f2c1f0ca3867")]
+[assembly: Guid("dd6687d9-664e-415a-95b6-12cec7a84e02")]
 
 // Version information for an assembly consists of the following four values:
 //
